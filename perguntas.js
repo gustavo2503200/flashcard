e4c9fe1553712458861d106136c195ee2c144c45
gustavo2503200 <00001093650266sp@al.educacao.sp.gov.br>
2024-10-16 11:07:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Historia',
+    'Em que ano foi criada a ONU?',
+    '1945'
+)
+criaCartao(
+    'matematica',
+    ' A unidade "grama" serve para medir:',
+    'massa'
+)
+criaCartao(
+    'Biologia',
+    ' Os fungos são organismos pertencentes a qual Reino?',
+    'Fungi'
+)
+criaCartao(
+    'Fisica',
+    ' o que é gravidade?',
+    'randeza responsável por definir o peso de um corpo, força vertical e para baixo que nos mantém unidos ao planeta'
+)
+
